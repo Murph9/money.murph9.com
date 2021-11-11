7836994942d8d9652f744c15ca9acf546276f3ca
Jake Murphy <murph9y@gmail.com>
2021-11-11 23:18:44 +1100

    bar graph view clean up buttons and cleanup current and future detection

--- src/components/bar-graph-view.tsx
+++ src/components/bar-graph-view.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 import Calc from "../utils/calc";
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 import DayTypeLib, { DayType } from "../utils/day-type";
 
 import Button from 'react-bootstrap/Button';
@@ -21,6 +25,7 @@ class BarGraphViewProps {
 
 const BarGraphView = (props: BarGraphViewProps) => {
 
+    const [periodType, setPeriodType] = React.useState(DayType.Day);
     const [periodOffset, setPeriodOffset] = React.useState(0);
     const incOffset = () => {
         setPeriodOffset(periodOffset + 1);
@@ -28,41 +33,56 @@ const BarGraphView = (props: BarGraphViewProps) => {
     const subOffset = () => {
         setPeriodOffset(periodOffset - 1);
     }
-
-    const type = DayType.Day;
     const futureCount = 2;
     const barCount = 7;
     const graphOffset = -barCount + 1 + futureCount;
-    const currentIndex = barCount-futureCount-1;
     
     const values = [];
-    const now = DayTypeLib.setToStart(new Date(), type);
+    const now = DayTypeLib.setToStart(new Date(), periodType);
     for (let i = 0; i < barCount; i++) {
-        const periodStart = DayTypeLib.offsetDateBy(now, type, i + graphOffset + periodOffset);
-        const value = props.calc.totalFor(type, periodStart);
-        values.push({ name: periodStart.toLocaleDateString(), amount: value, amountFormatted: `$${value.toFixed(2)}`, date: periodStart });
+        const periodStart = DayTypeLib.offsetDateBy(now, periodType, i + graphOffset + periodOffset);
+        const value = props.calc.totalFor(periodType, periodStart);
+        const obj: any = { name: periodStart.toLocaleDateString(), amount: value, amountFormatted: `$${value.toFixed(2)}`, date: periodStart };
+        if (now.getTime() == periodStart.getTime())
+            obj.current = 'Current';
+        else if (now.getTime() < periodStart.getTime())
+            obj.current = 'Future';
+        values.push(obj);
     }
     
-    //TODO calc fill colour based on currentIndex
-
     return (
     <div>
-        <Button variant="primary" onClick={subOffset}>-</Button>
-        
-        <Button variant="primary" onClick={incOffset}>+</Button>
+        <Container>
+            <Row>
+                <Col>
+                    <Button variant="primary" onClick={subOffset} style={{float: 'right'}}>-</Button>
+                </Col>
+                <Col>
+                <Form.Select value={periodType} onChange={(e) => setPeriodType(DayTypeLib.parseDayType(e.currentTarget.value))}>
+                    <option value={DayType.Day}>Day</option>
+                    <option value={DayType.Week}>Week</option>
+                    <option value={DayType.Month}>Month</option>
+                    <option value={DayType.Quarter}>Quarter</option>
+                    <option value={DayType.Year}>Year</option>
+                </Form.Select>
+                </Col>
+                <Col><Button variant="primary" onClick={incOffset} style={{float: 'left'}}>+</Button></Col>
+            </Row>
+        </Container>
         <ResponsiveContainer width="100%" height={300}>
             
             <BarChart data={values} margin={{ top: 10 }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <ReferenceLine y={0} stroke="#000" />
                 <Bar dataKey="amount" fill="#8884d8" isAnimationActive={false} onClick={(data, index) => {
-                    props.viewReport(type, data.date);
+                    props.viewReport(periodType, data.date);
                 }}>
                     {values.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={entry.amount >= 0 ? 'green': 'red'} strokeWidth={index === currentIndex ? 1 : 0} stroke='black'/>
+                        <Cell key={`cell-${index}`} fill={entry.amount >= 0 ? 'green': 'red'} />
                     ))}
                     <LabelList dataKey="name" position="insideBottom" style={{ fill: 'rgba(0, 0, 0, 0.87)' }}/>
                     <LabelList dataKey="amountFormatted" position="top" />
+                    <LabelList dataKey="current" position="center" style={{ fill: 'rgba(0, 0, 0, 0.87)' }}/>
                 </Bar>
             </BarChart>
             
