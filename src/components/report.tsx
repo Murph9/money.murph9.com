6e5b3121193ee24ad3be7724cb8277bd6558f533
Jake Murphy <murph9y@gmail.com>
2022-03-10 15:30:07 +1100

    show income graph and fix xaxis scale with added only

--- src/components/report.tsx
+++ src/components/report.tsx
@@ -45,6 +45,8 @@ const Report = (props: ReportProps) => {
 
     const [calcDiff, setCalcDiff] = React.useState(false);
     const [showAll, setShowAll] = React.useState(false);
+    const [showIncome, setShowIncome] = React.useState(false);
+
     const cur = props.calc.reportFor(props.type, props.date);
 
     let prev = new Map<string, number>();
@@ -89,6 +91,12 @@ const Report = (props: ReportProps) => {
         }
     }
 
+    let incomeList: Array<GraphRow> = [];
+    if (showIncome) {
+        incomeList = graphList.filter(x => x.income);
+        incomeList.forEach(x => {x.added *= -1; x.existing *= -1; x.removed *= -1});
+    }
+
     graphList = graphList.filter(x => !x.income);
     graphList.sort((x: GraphRow, y: GraphRow) => {
         return y.sum() - x.sum();
@@ -98,25 +106,51 @@ const Report = (props: ReportProps) => {
     });
 
     const graphCount = showAll ? graphList.length : 10;
+    let graphMax: number = 0;
+    if (graphList.length > 0) {
+        graphMax = graphList[0].sum();
+    }
     
     return (<>
         <div>
             <h4>Report for the {DayType[props.type]} of {props.date.toLocaleDateString()}</h4>
             <div style={{position: 'absolute', right: '0%', marginTop: -40}}>
+                <ButtonGroup><ToggleButton id="showincome-check" variant="outline-primary" value="1" type="checkbox" checked={showIncome} onChange={(e) => setShowIncome(e.currentTarget.checked)}>Show Income</ToggleButton></ButtonGroup>
                 <ButtonGroup><ToggleButton id="showall-check" variant="outline-primary" value="1" type="checkbox" checked={showAll} onChange={(e) => setShowAll(e.currentTarget.checked)}>Show All</ToggleButton></ButtonGroup>
                 <ButtonGroup><ToggleButton id="viewdiff-check" variant="outline-primary" value="1" type="checkbox" checked={calcDiff} onChange={(e) => setCalcDiff(e.currentTarget.checked)}>View Diff</ToggleButton></ButtonGroup>
                 <Button variant="secondary" onClick={props.closeCallback}>Close Breakdown</Button>
             </div>
         </div>
         
+        {showIncome && 
+            <ResponsiveContainer width="100%" height={incomeList.length*25+80}>
+                <BarChart data={incomeList} layout="vertical" id="a">
+                    <Legend verticalAlign="top" height={36}/>
+                    <XAxis dataKey='existing' type="number" domain={[0, 'auto']} />
+                    <YAxis dataKey='name' scale="band" type="category" width={150} interval={0} />
+                    <Bar dataKey="existing" isAnimationActive={false} fill="#8884d8" barSize={50} stackId="income" />
+                    {calcDiff &&
+                    <>
+                        <Bar dataKey="added" isAnimationActive={false} fill="#84d888" barSize={50} stackId="income" />
+                        <Bar dataKey="removed" isAnimationActive={false} fill="#844444" barSize={50} stackId="income" />
+                    </>
+                    }
+                    <Tooltip formatter={(value: number, name: string) => [value.toFixed(2), name]}/>
+                </BarChart>
+            </ResponsiveContainer>
+        }
+
         <ResponsiveContainer width="100%" height={graphCount*25+80}>
-            <BarChart data={graphList.slice(0, graphCount)} layout="vertical">
+            <BarChart data={graphList.slice(0, graphCount)} layout="vertical" id="b">
                 <Legend verticalAlign="top" height={36}/>
-                <XAxis dataKey='existing' type="number"/>
+                <XAxis dataKey='existing' type="number" domain={[0, graphMax]}/>
                 <YAxis dataKey='name' scale="band" type="category" width={150} interval={0} />
                 <Bar dataKey="existing" isAnimationActive={false} fill="#8884d8" barSize={50} stackId="main" />
-                <Bar dataKey="added" isAnimationActive={false} fill="#84d888" barSize={50} stackId="main" />
-                <Bar dataKey="removed" isAnimationActive={false} fill="#844444" barSize={50} stackId="main" />
+                {calcDiff &&
+                <>
+                    <Bar dataKey="added" isAnimationActive={false} fill="#84d888" barSize={50} stackId="main" />
+                    <Bar dataKey="removed" isAnimationActive={false} fill="#844444" barSize={50} stackId="main" />
+                </>}
                 <Tooltip formatter={(value: number, name: string) => [value.toFixed(2), name]}/>
             </BarChart>
         </ResponsiveContainer>
