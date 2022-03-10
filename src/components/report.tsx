11c71b9a979d5f4ba297eb9821d45055ee233bcb
Jake Murphy <murph9y@gmail.com>
2022-03-10 14:45:46 +1100

    don't load report diff by default

--- src/components/report.tsx
+++ src/components/report.tsx
@@ -41,25 +41,34 @@ class GraphRow {
 
 const Report = (props: ReportProps) => {
 
-    const prev = props.calc.reportFor(props.type, DayTypeLib.offsetDateBy(props.date, props.type, -1));
+    const [calcDiff, setCalcDiff] = React.useState(false);
     const cur = props.calc.reportFor(props.type, props.date);
 
+    let prev = new Map<string, number>();
+    if (calcDiff) {
+        prev = props.calc.reportFor(props.type, DayTypeLib.offsetDateBy(props.date, props.type, -1));
+    }
     let graphList: Array<GraphRow> = [];
     const names = [...new Set([...prev.keys(), ...cur.keys()])]; // get all names
     for (const name of names) {
-         if (!prev.has(name)) {
+        if (!calcDiff) {
+            let value = -cur.get(name);
+            graphList.push(new GraphRow(name, value));
+            continue;
+        }
+        if (!prev.has(name)) { // only added
             let value = -cur.get(name);
             let record = new GraphRow(name, 0);
             record.added = value;
             graphList.push(record);
         }
-        else if (!cur.has(name)) {
+        else if (!cur.has(name)) { // only removed
             let value = -prev.get(name);
             let record = new GraphRow(name, 0);
             record.removed = value;
             graphList.push(record);
         } else {
-            // both woo
+            // both
             let prevValue = -prev.get(name);
             let curValue = -cur.get(name);
             const dValue = Math.abs(curValue - prevValue);
@@ -88,8 +97,11 @@ const Report = (props: ReportProps) => {
     
     return (<>
         <div>
-            <h4 style={{textAlign: 'center'}}>Report for the {DayType[props.type]} of {props.date.toLocaleDateString()}</h4>
-            <Button style={{position: 'absolute', right: '0%', marginTop: -40}} onClick={props.closeCallback}>Close Breakdown</Button>
+            <h4>Report for the {DayType[props.type]} of {props.date.toLocaleDateString()}</h4>
+            <div style={{position: 'absolute', right: '0%', marginTop: -40}}>
+                <Button onClick={() => setCalcDiff(!calcDiff)}>View Diff</Button>
+                <Button variant="secondary" onClick={props.closeCallback}>Close Breakdown</Button>
+            </div>
         </div>
         
         <ResponsiveContainer width="100%" height={Math.min(graphList.length*25, 600)}>
@@ -104,31 +116,33 @@ const Report = (props: ReportProps) => {
             </BarChart>
         </ResponsiveContainer>
 
-        <Table size="sm">
-            <thead>
-                <tr><th>Category Differences</th><th>Prev period</th><th>Current</th><th>Diff</th></tr>
-            </thead>
-            <tbody>
-                {names.map(x => {
-                    return{
-                        name: x,
-                        prevV: (-prev.get(x) || 0),
-                        curV: (-cur.get(x) || 0),
-                        dValue: (-cur.get(x) || 0) - (-prev.get(x) || 0)
-                    };
-                })
-                    .filter(x => x.dValue)
-                    .sort((x,y) => x.name.localeCompare(y.name))
-                    .map(x => 
-                        <tr>
-                            <td>{x.name}</td>
-                            <td>${x.prevV.toFixed(2)}</td>
-                            <td>${x.curV.toFixed(2)}</td>
-                            <td>${x.dValue.toFixed(2)}</td>
-                        </tr>
-                    )}
-            </tbody>
-        </Table>
+        {calcDiff && 
+            <Table size="sm">
+                <thead>
+                    <tr><th>Category Differences</th><th>Prev period</th><th>Current</th><th>Diff</th></tr>
+                </thead>
+                <tbody>
+                    {names.map(x => {
+                        return{
+                            name: x,
+                            prevV: (-prev.get(x) || 0),
+                            curV: (-cur.get(x) || 0),
+                            dValue: (-cur.get(x) || 0) - (-prev.get(x) || 0)
+                        };
+                    })
+                        .filter(x => x.dValue)
+                        .sort((x,y) => x.name.localeCompare(y.name))
+                        .map(x => 
+                            <tr>
+                                <td>{x.name}</td>
+                                <td>${x.prevV.toFixed(2)}</td>
+                                <td>${x.curV.toFixed(2)}</td>
+                                <td>${x.dValue.toFixed(2)}</td>
+                            </tr>
+                        )}
+                </tbody>
+            </Table>
+        }
     </>
     );
 }
