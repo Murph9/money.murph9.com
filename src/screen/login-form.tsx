e8a0a8a9300b07bb4c595fc018221cc8529ec45b
Jake Murphy <murph9y@gmail.com>
2022-05-23 20:07:58 +1000

    remove unused using

--- src/screen/login-form.tsx
+++ src/screen/login-form.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
